efc23f3755af30900698fe609da46f2881fcba91
wangcm <779048161@qq.com>
2021-06-06 20:25:43 +0800

    添加很多代码

---
+++ src/test/java/com/liangyu/springboot_testgit/SpringTest.java
@@ -0,0 +1,17 @@
+package com.liangyu.springboot_testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest {
+
+    @Test
+    void contextLoads() {
+        int a=8;
+        int b=10;
+        System.out.println("测试用的");
+        System.out.println("nishuone");
+    }
+
+}
