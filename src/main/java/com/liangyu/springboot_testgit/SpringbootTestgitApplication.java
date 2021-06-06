1f70d479fb713f71d8cd9c03d73afe3ff49d5001
wangcm <779048161@qq.com>
2021-06-06 20:12:27 +0800

    ceshispring_boot基础的环境v1.0

---
+++ src/main/java/com/liangyu/springboot_testgit/SpringbootTestgitApplication.java
@@ -0,0 +1,13 @@
+package com.liangyu.springboot_testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestgitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liangyu/springboot_testgit/SpringbootTestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liangyu.springboot_testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
